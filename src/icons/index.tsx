f064d879bdbc423a903720bd98f746b3bb768958
mnewen-a11y <mnewen@googlemail.com>
2026-02-19 07:49:06 +0100

    Delete src/icons/index.tsx

--- src/icons/index.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Time of Day Icons
- * Minimalistische SVG Icons für Tageszeiten
- */
-
-export { SunriseIcon } from './SunriseIcon';
-export { SunIcon } from './SunIcon';
-export { SunHazeIcon } from './SunHazeIcon';
-export { MoonIcon } from './MoonIcon';
