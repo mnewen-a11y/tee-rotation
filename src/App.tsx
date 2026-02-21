5d29b8f41e0d9cb3dcbe490223a7a403c40b117b
mnewen-a11y <mnewen@googlemail.com>
2026-02-21 10:46:31 +0100

    Reduce top padding from 2rem to 1rem
    paddingTop: '2rem', => paddingTop: '1rem',

--- src/App.tsx
+++ src/App.tsx
@@ -366,7 +366,7 @@ function App() {
             </div>
           ) : (
             <div className="flex flex-col touch-pan-x items-center justify-center" style={{ 
-              paddingTop: '2rem',
+              paddingTop: '1rem',
               paddingBottom: 'max(0.5rem, env(safe-area-inset-bottom))',
               minHeight: 'calc(100vh - 60px)',
               overscrollBehavior: 'none'
